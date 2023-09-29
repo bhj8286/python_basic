763b4626fb1c5dd14689480fe5701a6cd5eb31bd
bhj8286 <bjs219@naver.com>
2023-09-29 22:53:58 +0900

    update

--- practice2.py
+++ practice2.py
@@ -8,8 +8,16 @@ back_number = ['10', '7', '4', '9']
 
 numbers = range(1, 46)
 lucky_number = random.sample(numbers, 6)
-print(lucky_number)
+# print(lucky_number)
 
 numbers = range(1, 46)
 lucky_number = random.sample(numbers, 6)
-print(sorted(lucky_number))
+# print(sorted(lucky_number))
+
+sports_game = ['soccer', 'baseball', 'basketball']
+random_number = random.randint(0, 2)
+print(sports_game[random_number])
+
+numbers = [1, 2, 3, 4]
+max_naumber = max(numbers)
+print(max_naumber)
