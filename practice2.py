db4fb8d5a34f82e68cb63372d25c8e7260fdc05a
bhj8286 <bjs219@naver.com>
2023-09-29 22:31:42 +0900

    update

--- practice2.py
+++ practice2.py
@@ -3,7 +3,13 @@ animals = ['dog', 'cat', 'tiger', 'lion']
 random_number = random.randint(0,3)
 # print(animals[random_number]) 
 
-lists = ['A', 'B', 'c']
+back_number = ['10', '7', '4', '9']
+# print(back_number)
 
-for list in lists:
-    print(list)
+numbers = range(1, 46)
+lucky_number = random.sample(numbers, 6)
+print(lucky_number)
+
+numbers = range(1, 46)
+lucky_number = random.sample(numbers, 6)
+print(sorted(lucky_number))
