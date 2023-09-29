e23e86299308ffd4543274353cff6528fdd0fba0
bhj8286 <bjs219@naver.com>
2023-09-29 22:05:24 +0900

    update

--- practice2.py
+++ practice2.py
@@ -1,4 +1,9 @@
 import random
 animals = ['dog', 'cat', 'tiger', 'lion']
 random_number = random.randint(0,3)
-print(animals[random_number]) 
+# print(animals[random_number]) 
+
+lists = ['A', 'B', 'c']
+
+for list in lists:
+    print(list)
